4df59b761deac8ebc4abb84fee6f473ec3700c83
ZELAL BAŞAK AKYOL <akyolzelalbasak@outlook.com>
2021-02-23 14:37:33 +0300

    .

---
+++ Oop2/MusteriManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    // İş sınıflarıdır // business classes
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+        }
+    }
+}
